4a8cb544a0a75c3b12c5143a45c5488ba850f440
Eric M <ericmarchese77@gmail.com>
2015-10-04 22:00:44 -0400

    Completed working cipher method

---
+++ caesar_cipher.rb
@@ -0,0 +1,25 @@
+def caesar_cipher(string, shift_factor)
+  alphabet = ("a".."z").to_a
+  ciphered_alphabet = alphabet.dup
+  new_string = ""
+  while shift_factor > 0
+    ciphered_alphabet << ciphered_alphabet.shift
+    shift_factor -= 1
+  end
+  split_string = string.split("")
+  split_string.each do |letter|
+    alpha_index = alphabet.index(letter.downcase)
+    if alpha_index.nil?
+      new_string << letter
+    else
+      if letter == letter.downcase
+        new_string << ciphered_alphabet[alpha_index]
+      else
+        new_string << ciphered_alphabet[alpha_index].capitalize
+      end
+    end
+  end
+  new_string
+end
+
+puts caesar_cipher("What a string!", 5)
